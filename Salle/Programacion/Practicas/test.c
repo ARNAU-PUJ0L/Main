c7c5cff54af6227cc44e72fb7834a0607896bdd9
ARNAU-PUJ0L <96207351+ARNAU-PUJ0L@users.noreply.github.com>
2023-10-20 23:55:44 +0200

    .

--- Salle/Programacion/Practicas/test.c
+++ Salle/Programacion/Practicas/test.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    char nombre[50];  // Creamos un arreglo de caracteres para almacenar el nombre (puedes ajustar el tamaño)
-    int edad;
 
-    // Solicitamos el nombre al usuario
-    printf("Por favor, introduce tu nombre: ");
-    scanf("%s", nombre);
+    int num;
+    int i = 1;
+    int j = 1;
 
-    // Solicitamos la edad al usuario
-    printf("Ahora, introduce tu edad: ");
-    scanf("%d", &edad);
+    printf("Num enter positiu: ");
+    scanf("%d", &num);
 
-    // Mostramos el nombre y la edad en pantalla
-    printf("Tu nombre es: %s\n", nombre);
-    printf("Tienes %d años de edad.\n", edad);
+    while ( i <= num){
+        
+        if( i % j != 0){
+
+            j++;
+
+        }
+
+        printf("%d", i);
+        i++;
+
+    }
+    
+
 
     return 0;
 }
