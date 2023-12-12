9340e825d00d9e5c3e7e52205335972b18833b14
ARNAU-PUJ0L <96207351+ARNAU-PUJ0L@users.noreply.github.com>
2023-12-12 18:35:38 +0100

    .

---
+++ Salle/Algebra_Practicas/Practica_1/P1_NomCognom_Catala.m
@@ -0,0 +1,71 @@
+function [a,b,c,Determinant,Resultat,Resposta,Rang,X1,Tipus1,X2,Tipus2,X3,Base,Bc_B1,dB1,eB2,B2_B3,eB3] = P1_NomCognom_Catala()
+%% Practica 1. EXERCICIS MATLAB. ÀLGEBRA LINEAL
+
+%% NOM I COGNOMS DELS MEMBRES DEL GRUP
+%Membre 1:
+%Membre 2:
+%Membre 3:
+%Grup: A, B, C, D, E, F
+
+%Per a executar el codi, cal copiar la linia 1 d'aquest fitxer a la línia
+%de comandes (sense la paraula function)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%                             TEMA 1                              %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Exercici 1 
+% a =   
+% b =   
+% c =   
+
+%Exercici 2 
+% Determinant = 
+
+%Exercici 3 
+% Resultat = 
+% Resposta = ""
+
+%Exercici 4 
+% Rang = 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%                             TEMA 2                              %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Exercici 1
+% X1 = 
+% Tipus1 = 
+
+%Exercici 2
+% X2 = 
+% Tipus2 = 
+    
+%Exercici 3
+% X3 = 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%                             TEMA 3                              %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Exercici 1 
+% Base = 
+
+%Exercici 2
+    %a) 
+% d = 
+    %b) 
+% Bc_B1=
+    %c)
+% dB1= 
+
+%Exercici 3
+    %a)
+% eB2 = 
+    %b)
+% B2_B3 = 
+    %c)
+% eB3 = 
+end
+
+
+
+
+
