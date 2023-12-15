14a07b3f0643be369b56f441fa98daa53dea35c7
ARNAU-PUJ0L <96207351+ARNAU-PUJ0L@users.noreply.github.com>
2023-12-15 23:34:41 +0100

    .

--- Salle/Algebra_Practicas/Practica_1/P1_NomCognom_Catala.m
+++ Salle/Algebra_Practicas/Practica_1/P1_NomCognom_Catala.m
@@ -1,5 +1,7 @@
 function [a,b,c,Determinant,Resultat,Resposta,Rang,X1,Tipus1,X2,Tipus2,X3,Base,Bc_B1,dB1,eB2,B2_B3,eB3] = P1_NomCognom_Catala()
 
+d = [2; 1; 5];
+
 B1 = [3, 2, 3; 1, 5, 4; 2, 1, 0];
 
 %b) 
@@ -7,9 +9,14 @@ Bc = [1, 0, 0;
       0, 1, 0;
       0, 0, 1];
 
-inversa= inv(B1);
+Bc_B1 = inv(B1) * Bc;
+disp(Bc_B1);
 
-Bc_B1 = inversa' * Bc;
+comp_Bc = Bc \ d;
+disp (comp_Bc);
+
+comp_B1 = Bc_B1 * comp_Bc;
+disp(comp_B1);
 
 %c)
 
