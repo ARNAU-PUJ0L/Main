13e29acd4e1bb456b87ce40e4d6b7c9d0ca196c4
ARNAU-PUJ0L <96207351+ARNAU-PUJ0L@users.noreply.github.com>
2024-02-06 12:31:24 +0100

    .

--- Salle/Programacio/Practicas/P-2/parking.c
+++ Salle/Programacio/Practicas/P-2/parking.c
@@ -7,6 +7,31 @@
 #define MAX_VEHICLES 8
 #define MAX_OPERATIONS 10
 
+/* ERROR
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+typedef struct {
+    
+    char type;
+    char plate[MAX_COMAND];
+    char enter_hour[MAX_COMAND];
+    char exit_hour[MAX_COMAND];
+
+
+}vehicle;
+
 
 
 void fix_str(char string[MAX_TARIF]){
@@ -41,75 +66,100 @@ int identify_comand(char str_1[MAX_COMAND], char str_2[MAX_COMAND]){
 
 
 
-
-
 void enter(char comand[MAX_COMAND]){
 
-    int valid = 0;
-    int wrong_command = 0;
-    int error_time = 0;
+
+    int valid = 1;
     int len_comand = strlen(comand);
+    int num_vehicles = 0;
 
 
-    //Es correcto el comand?
+    do{
 
-    
-    if((comand[0] == 'e' && comand[1] == 'n' && comand[2] == 't' && comand[3] == 'e' && comand[4] == 'r') && (comand[5] == ' ' && (comand[6] == 'T' || comand[6] == 'C' || comand[6] == 'B') && comand[7] == ' ') 
-    && (comand[8] >= '0' && comand[8] <= '9') && (comand[9] >= '0' && comand[9] <= '9') && (comand[10] >= '0' && comand[10] <= '9') && (comand[11] >= '0'  && comand[11] <= '9') 
-    && (comand[12] >= 'A' && comand[12] <= 'Z')  && (comand[13] >= 'A' && comand[13] <= 'Z')  && (comand[14] >= 'A' && comand[14] <= 'Z') ){
+        if(valid == 0){
+            printf("\n\nParking LS> ");
+            fgets(comand, MAX_COMAND, stdin);
+            fix_str(comand);   
+            len_comand = strlen(comand);     
+        }
+        
+        if((comand[0] == 'e' && comand[1] == 'n' && comand[2] == 't' && comand[3] == 'e' && comand[4] == 'r') && (comand[5] == ' ' && (comand[6] == 'T' || comand[6] == 'C' || comand[6] == 'B') && comand[7] == ' ') 
+        && (comand[8] >= '0' && comand[8] <= '9') && (comand[9] >= '0' && comand[9] <= '9') && (comand[10] >= '0' && comand[10] <= '9') && (comand[11] >= '0'  && comand[11] <= '9') 
+        && (comand[12] >= 'A' && comand[12] <= 'Z')  && (comand[13] >= 'A' && comand[13] <= 'Z')  && (comand[14] >= 'A' && comand[14] <= 'Z') ){
 
-        // Hora
-        switch (len_comand){
+            // MAtricula == commad[8] - commmand[14]
+            // PISTA -> el maximo de coches q pueden entrar es 8
+
+
+
+
+            // Hora
+            switch (len_comand){
+                                
+                case 21:
+
+                    if(comand[16] == '2'){
+
+                        if((comand[17] >= '0' && comand[17] <= '4') && comand[18] == ':' && (comand[19] >= '0' && comand[19] <= '5') && (comand[20] >= 0 && comand[20] <= '9')){
+                            valid = 1;
+                        }else{
+                            printf("(ERROR) Wrong time format\n");
+                            valid = 0;
+                        }
+                    }
+
+                    if(comand[16] == '1'){
+
+                        if((comand[17] >= '0' && comand[17] <= '9') && comand[18] == ':' && (comand[19] >= '0' && comand[19] <= '5') && (comand[20] >= 0 && comand[20] <= '9')){
+                            valid = 1;
+                        }else{
+                            printf("(ERROR) Wrong time format\n");
+                            valid = 0;
+                        }
+                    
+                    }
+
+                    if(comand[16] != '1' && comand[16] != '2'){
+                        printf("(ERROR) Wrong time format\n");
+                        valid = 0;
+                    }
+
+                    break;
                             
-            case 21:
-
-                if(comand[16] == '2'){
-
-                    if((comand[17] >= '0' && comand[17] <= '4') && comand[18] == ':' && (comand[19] >= '0' && comand[19] <= '5') && (comand[20] >= 0 && comand[20] <= '9')){
+                case 20:
+                    
+                    if((comand[16] >= '0' && comand[16] <= '9') && comand[17] == ':' && (comand[18] >= '0' && comand[18] <= '5') && (comand[19] >= 0 && comand[19] <= '9')){
                         valid = 1;
                     }else{
-                        error_time = 1;
+                        printf("(ERROR) Wrong time format\n");
+                        valid = 0;
                     }
-                }
 
-                if(comand[16] == '1'){
+                    break;
 
-                    if((comand[17] >= '0' && comand[17] <= '9') && comand[18] == ':' && (comand[19] >= '0' && comand[19] <= '5') && (comand[20] >= 0 && comand[20] <= '9')){
-                        valid = 1;
-                    }else{
-                        error_time = 1;
-                    }
-                
-                }
+                default:
+                    printf("(ERROR) Wrong command\n");
+                    valid = 0;
+                    
+                    break;
+            }
 
-                if(comand[16] != '1' && comand[16] != '2'){
-                    error_time = 1;
-                }
-
-                break;
-                        
-            case 20:
-                
-                if((comand[16] >= '0' && comand[16] <= '9') && comand[17] == ':' && (comand[18] >= '0' && comand[18] <= '5') && (comand[19] >= 0 && comand[19] <= '9')){
-                    valid = 1;
-                }else{
-                    error_time = 1;
-                }
-
-                break;
+        }else{
+            printf("(ERROR) Wrong command\n");
+            valid = 0;
         }
 
-    }else{
-        wrong_command = 1;
-    }
-                // PONER PRINT DIRECTAMNETE
-    if(wrong_command = 1){
-        printf("(ERROR) Wrong command\n");
+
+    }while(valid == 0);
+
+    num_vehicles++;
+
+    if (num_vehicles > MAX_VEHICLES){
+        printf("(ERROR) No more vehicles are accepted");
+        num_vehicles = MAX_VEHICLES;  // NS PQ
     }
 
-    if(error_time == 1){
-        printf("(ERROR) Wrong time format\n");
-    }
+
 
 
 
@@ -156,29 +206,41 @@ int main(){
 
     // CLASIFICAR COMANDOS
 
-/// MAL
+// ERROR'S time and command
     do{
         printf("\nParking LS> ");
         fgets(comand, MAX_COMAND, stdin);
         fix_str(comand);
 
         int_enter = identify_comand(comand, str_enter);
-        //printf("\n%d", int_enter ); 
+        
+        if(int_enter == 0){
+            enter(comand);
+        }
         
         int_exit = identify_comand(comand, str_exit);
-        //printf("\n%d",int_exit ); 
+
+        if(int_exit == 0){
+            //
+        }
 
         int_show = identify_comand(comand, str_show);
-        //printf("\n%d", int_show); 
+        if(int_show == 0){
+            //
+        }
 
         int_quit = identify_comand(comand, str_quit);
-        //printf("\n%d",int_quit ); 
+        if(int_quit == 0){
+            //
+        }
 
-    }while(int_enter != 0 && int_exit != 0 && int_show != 0 && int_quit != 0);
+    }while(int_quit != 0);
 
-///
+    printf("See you later!");
 
 
+
+/*
     // Distrivucion de funciones
 
     if(int_enter == 0){
@@ -198,6 +260,6 @@ int main(){
         return 0;
     }
 
-
+*/
     return 0;
 }
--- Salle/Programacio/Teoria/Files/Files.c
+++ Salle/Programacio/Teoria/Files/Files.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+
+Modes fichers text: r -> read; w -> write; a -> append
+Modes fichers binaris: rb; wb; ab;
+
+
+*/
+
+
 int main() {
 
     char mensaje[100];
@@ -25,6 +34,12 @@ int main() {
 
             fptr = fopen("data.txt", "w");
 
+            if (fptr == NULL){
+                printf("ERROR DE LECTURA");
+                return 1;
+            }
+
+
             fprintf(fptr, "Tu nombre es %s y tienes %d anyos", name, age);
             fclose(fptr);
 
@@ -33,6 +48,12 @@ int main() {
         case 'o':
             
             fptr = fopen("data.txt", "r");
+
+            if (fptr == NULL){
+                printf("ERROR DE LECTURA");
+                return 1;
+            }
+
             fgets(mensaje, 100, fptr);
             printf("%s", mensaje);
             fclose(fptr);
