7bbd6823c44e869d328f7681d6af34d8a8640846
ARNAU-PUJ0L <96207351+ARNAU-PUJ0L@users.noreply.github.com>
2024-02-25 19:37:14 +0100

    .

--- Salle/Programacio/Practicas/P-2/parking.c
+++ Salle/Programacio/Practicas/P-2/parking.c
@@ -1,3 +1,13 @@
+/**************************************************************************************************************************************
+* @ Proposit: ----------------------------------------------------
+
+*
+* @ Autor: Arnau Pujol Chaparro
+* @ Data de Creació: 28/1/23
+* @ Data de ultima modificació: --------------------------------------------
+**************************************************************************************************************************************/
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -7,6 +17,8 @@
 #define MAX_PRICE 15
 
 #define MAX 10
+#define MAX_PLATE 15
+#define MAX_TIME 4
 
 #define MAX_VEHICLES 8
 #define MAX_OPERATIONS 10
@@ -35,82 +47,252 @@ int identify_comand(char str_1[MAX_COMAND], char str_2[MAX_COMAND]){
 }
 
 
-
-
-
-void read_tariff(char tariffs[MAX_TARIF]){
-   
-    char truck[MAX] = "TRUCKS";
-    char car[MAX] = "CAR";
-    char bikes[MAX] = "BIKES";
-
+int read_trucks(char tariffs[MAX_TARIF], int i){
+    
+    int z = 0;
     int j = 0;
-    int i = 0;
     int k = 0;
-    
-    char str_price_reduced[MAX_PRICE];
-    char str_price_peak[MAX_PRICE];
-    char str_price_normal[MAX_PRICE];
-    
+
+
     int truck_price_reduced = 0;
     int truck_price_peak = 0;
     int truck_price_normal = 0;
+
+
+    char str_price_reduced[MAX_PRICE];
+    char str_price_peak[MAX_PRICE];
+    char str_price_normal[MAX_PRICE];
+
+    while(tariffs[i] != ':'){
+        i++;
+    }
+
+    i++;
+
+    for(j = 0; j < MAX_PRICE && tariffs[i] != '/'; j++, i++){
+        str_price_reduced[j] = tariffs[i];
+    }
+        
+    str_price_reduced[j] = '\0';
+    truck_price_reduced = atoi(str_price_reduced);
+
+
+    i++;
+
+    for(j = 0; j < MAX_PRICE && tariffs[i] != '/'; j++, i++){
+        str_price_normal[j] = tariffs[i];
+    }
+
+    str_price_normal[j] = '\0';
+    truck_price_normal = atoi(str_price_normal);
+
+    i++;
+
+    for(j = 0; j < MAX_PRICE && (tariffs[i] != '#' && tariffs[i] != '\0'); j++, i++){
+        str_price_peak[j] = tariffs[i];
+    }
+
+    str_price_peak[j] = '\0';
+    truck_price_peak = atoi(str_price_peak);
+
+
+    while(tariffs[i] != 'C' && tariffs[i] != 'B' && tariffs[i] != '\0'){
+        i++;
+    }
+
+    return i;
+
+}
+
+int read_cars(char tariffs[MAX_TARIF], int i){
+    
+    int z = 0;
+    int j = 0;
+    int k = 0;
 
     int car_price_reduced = 0;
     int car_price_peak = 0;
     int car_price_normal = 0;
 
+    char str_price_reduced[MAX_PRICE];
+    char str_price_peak[MAX_PRICE];
+    char str_price_normal[MAX_PRICE];
+
+    while(tariffs[i] != ':'){
+        i++;
+    }
+
+    i++;
+
+    for(j = 0; j < MAX_PRICE && tariffs[i] != '/'; j++, i++){
+        str_price_reduced[j] = tariffs[i];
+    }
+        
+    str_price_reduced[j] = '\0';
+    car_price_reduced = atoi(str_price_reduced);
+
+    i++;
+
+    for(j = 0; j < MAX_PRICE && tariffs[i] != '/'; j++, i++){
+        str_price_normal[j] = tariffs[i];
+    }
+
+    str_price_normal[j] = '\0';
+    car_price_normal = atoi(str_price_normal);
+
+    i++;
+
+    z = i;
+
+    for(j = 0; j < MAX_PRICE && (tariffs[z] != '#' || tariffs[z] != '\0'); j++, z++){
+        str_price_peak[j] = tariffs[z];
+    }
+
+    str_price_peak[j] = '\0';
+    car_price_peak = atoi(str_price_peak);
+
+
+    while(tariffs[i] != 'B' && tariffs[i] != 'T' && tariffs[i] != '\0'){
+        i++;
+    }
+
+
+    return i;
+
+}
+
+int read_bikes(char tariffs[MAX_TARIF], int i){
+    
+    int j = 0;
+    int k = 0;
+
+    int z = 0;
+
+
     int bike_price_reduced = 0;
     int bike_price_peak = 0;
     int bike_price_normal = 0;
 
+    char str_price_reduced[MAX_PRICE];
+    char str_price_peak[MAX_PRICE];
+    char str_price_normal[MAX_PRICE];
+
+    while(tariffs[i] != ':'){
+        i++;
+    }
+
+    i++;
+
+    for(j = 0; j < MAX_PRICE && tariffs[i] != '/'; j++, i++){
+        str_price_reduced[j] = tariffs[i];
+    }
+        
+    str_price_reduced[j] = '\0';
+    bike_price_reduced = atoi(str_price_reduced);
+
+    i++;
+
+    for(j = 0; j < MAX_PRICE && tariffs[i] != '/'; j++, i++){
+        str_price_normal[j] = tariffs[i];
+    }
+
+    str_price_normal[j] = '\0';
+    bike_price_normal = atoi(str_price_normal);
+
+    i++;
+
+    z = i;
+
+    for(j = 0; j < MAX_PRICE && (tariffs[z] != '#' || tariffs[z] != '\0'); j++, z++){
+        str_price_peak[j] = tariffs[z];
+    }
+
+    str_price_peak[j] = '\0';
+    bike_price_peak = atoi(str_price_peak);
+
+    while(tariffs[i] != 'T' && tariffs[i] != 'C' && tariffs[i] != '\0'){
+        i++;
+    }
+    return i;
+}
+
+
+
+
+void read_tariff(char tariffs[MAX_TARIF]){
+    
+    char truck[MAX] = "TRUCKS";
+    char car[MAX] = "CARS";
+    char bikes[MAX] = "BIKES";
+
+    int b = 0;
+    int b2 = 0;
+
+    int c = 0;
+    int c2 = 0;
+
+    int t = 0;
+    int t2 = 0;
+
 
     if(identify_comand(truck, tariffs) == 0){
 
-        while(tariffs[i] != ':'){
-            i++;
+        t = read_trucks(tariffs, 0);
+
+        if(tariffs[t] == 'C'){
+            t2 = read_cars(tariffs, t);
         }
 
-        i++;
-
-        for(j = 0; j < MAX_PRICE && tariffs[i] != '/'; j++, i++){
-            str_price_reduced[j] = tariffs[i];
-        }
-        
-        str_price_reduced[j] = '\0';
-        truck_price_reduced = atoi(str_price_reduced);
-
-        i++;
-
-        for(j = 0; j < MAX_PRICE && tariffs[i] != '/'; j++, i++){
-            str_price_normal[j] = tariffs[i];
+        if(tariffs[t] == 'B'){
+            t2 = read_bikes(tariffs, t);
         }
 
-        str_price_normal[j] = '\0';
-        truck_price_normal = atoi(str_price_normal);
-
-        i++;
-
-        for(j = 0; j < MAX_PRICE && (tariffs[i] != '#' || tariffs[i] != '\0'); j++, i++){
-            str_price_peak[j] = tariffs[i];
+        if(tariffs[t2] == 'C'){
+            read_cars(tariffs, t2);
+        }else{
+            read_bikes(tariffs, t2);
         }
-
-        str_price_peak[j] = '\0';
-        truck_price_peak = atoi(str_price_peak);
-
-
-        printf("R:%d || N: %d || P: %d", truck_price_reduced, truck_price_normal, truck_price_peak);
-
     }
 
 
+    if(identify_comand(bikes, tariffs) == 0){
+
+        b = read_bikes(tariffs, 0);
 
 
+        if(tariffs[b] == 'C'){
+            b2 = read_cars(tariffs, b);
+        }
 
+        if(tariffs[b] == 'T'){
+            b2 = read_trucks(tariffs, b);
+        }
 
+        if(tariffs[b2] == 'C'){
+            read_cars(tariffs, b2);
+        }else{
+            read_trucks(tariffs, b2);
+        }
+    }
 
+    if(identify_comand(car, tariffs) == 0){
 
+        c = read_cars(tariffs, 0);
 
+        if(tariffs[c] == 'T'){
+            c2 = read_trucks(tariffs, c);
+        }
+
+        if(tariffs[c] == 'B'){
+            c2 = read_bikes(tariffs, c);
+        }
+
+        if(tariffs[c2] == 'T'){
+            read_trucks(tariffs, c2);
+        }else{
+            read_bikes(tariffs, c2);
+        }
+    }
 
 }
 
@@ -119,97 +301,59 @@ void read_tariff(char tariffs[MAX_TARIF]){
 
 
 
-void enter(char comand[MAX_COMAND]){
+void enter(char command[MAX_COMAND]){
 
+    char time[MAX_TIME];
+    char vehicle;
+    char plate[MAX_PLATE];
+    int i = 0;
+    int j = 0;
 
-    int valid = 1;
-    int len_comand = strlen(comand);
-    int num_vehicles = 0;
-
-    do{
-
-        if(valid == 0){
-            printf("\n\nParking LS> ");
-            fgets(comand, MAX_COMAND, stdin);
-            fix_str(comand);   
-            len_comand = strlen(comand);     
-        }
-        
-        if((comand[0] == 'e' && comand[1] == 'n' && comand[2] == 't' && comand[3] == 'e' && comand[4] == 'r') && (comand[5] == ' ' && (comand[6] == 'T' || comand[6] == 'C' || comand[6] == 'B') && comand[7] == ' ') 
-        && (comand[8] >= '0' && comand[8] <= '9') && (comand[9] >= '0' && comand[9] <= '9') && (comand[10] >= '0' && comand[10] <= '9') && (comand[11] >= '0'  && comand[11] <= '9') 
-        && (comand[12] >= 'A' && comand[12] <= 'Z')  && (comand[13] >= 'A' && comand[13] <= 'Z')  && (comand[14] >= 'A' && comand[14] <= 'Z') ){
-
-            // MAtricula == commad[8] - commmand[14]
-            // PISTA -> el maximo de coches q pueden entrar es 8
-
-
-
-
-            // Hora
-            switch (len_comand){
-                                
-                case 21:
-
-                    if(comand[16] == '2'){
-
-                        if((comand[17] >= '0' && comand[17] <= '4') && comand[18] == ':' && (comand[19] >= '0' && comand[19] <= '5') && (comand[20] >= 0 && comand[20] <= '9')){
-                            valid = 1;
-                        }else{
-                            printf("(ERROR) Wrong time format\n");
-                            valid = 0;
-                        }
-                    }
-
-                    if(comand[16] == '1'){
-
-                        if((comand[17] >= '0' && comand[17] <= '9') && comand[18] == ':' && (comand[19] >= '0' && comand[19] <= '5') && (comand[20] >= 0 && comand[20] <= '9')){
-                            valid = 1;
-                        }else{
-                            printf("(ERROR) Wrong time format\n");
-                            valid = 0;
-                        }
-                    
-                    }
-
-                    if(comand[16] != '1' && comand[16] != '2'){
-                        printf("(ERROR) Wrong time format\n");
-                        valid = 0;
-                    }
-
-                    break;
-                            
-                case 20:
-                    
-                    if((comand[16] >= '0' && comand[16] <= '9') && comand[17] == ':' && (comand[18] >= '0' && comand[18] <= '5') && (comand[19] >= 0 && comand[19] <= '9')){
-                        valid = 1;
-                    }else{
-                        printf("(ERROR) Wrong time format\n");
-                        valid = 0;
-                    }
-
-                    break;
-
-                default:
-                    printf("(ERROR) Wrong command\n");
-                    valid = 0;
-                    
-                    break;
-            }
-
-        }else{
-            printf("(ERROR) Wrong command\n");
-            valid = 0;
-        }
-
-
-    }while(valid == 0);
-
-    num_vehicles++;
-
-    if (num_vehicles > MAX_VEHICLES){
-        printf("(ERROR) No more vehicles are accepted");
-        num_vehicles = MAX_VEHICLES;  // NS PQ
+    while(command[i] != ' '){
+        i++;
     }
+
+    i++;
+
+    switch (command[i]){
+        case 'C':
+            vehicle = 'C';
+            break;
+        
+        case 'T':
+            vehicle = 'T';
+            break;
+
+        case 'B':
+            vehicle = 'B';
+            break;
+
+        default:
+            printf("(ERROR) Wrong command");
+            break;
+    }
+
+    i + 2;
+
+    for(j = 0; j < MAX_COMAND && command[i] != ' '; j++, i++){
+        plate[j] = command[i];
+    }
+
+    plate[j] = '\0';
+
+    i++;
+
+    for(j = 0; j < MAX_TIME && command[i] != '\0'; j++, i++){
+        time[j] = command[i];
+    }
+
+    time[j] = '\0';
+    
+
+
+
+
+
 
 }
 
@@ -225,7 +369,7 @@ int main(){
     // Variables
     
     char tariffs[MAX_TARIF];
-    char comand[MAX_COMAND];
+    char command[MAX_COMAND];
 
     //menu
     int int_enter = 1;
@@ -252,32 +396,31 @@ int main(){
 
     read_tariff(tariffs);
 
-    // CLASIFICAR COMANDOS
 
 // ERROR'S time and command
     do{
         printf("\nParking LS> ");
-        fgets(comand, MAX_COMAND, stdin);
-        fix_str(comand);
+        fgets(command, MAX_COMAND, stdin);
+        fix_str(command);
 
-        int_enter = identify_comand(comand, str_enter);
+        int_enter = identify_comand(command, str_enter);
         
         if(int_enter == 0){
-            enter(comand);
+            enter(command);
         }
         
-        int_exit = identify_comand(comand, str_exit);
+        int_exit = identify_comand(command, str_exit);
 
         if(int_exit == 0){
             //
         }
 
-        int_show = identify_comand(comand, str_show);
+        int_show = identify_comand(command, str_show);
         if(int_show == 0){
             //
         }
 
-        int_quit = identify_comand(comand, str_quit);
+        int_quit = identify_comand(command, str_quit);
         if(int_quit == 0){
             //
         }
@@ -287,27 +430,5 @@ int main(){
     printf("See you later!");
 
 
-
-/*
-    // Distrivucion de funciones
-
-    if(int_enter == 0){
-        enter(comand);
-    }
-
-    if(int_exit == 0){
-        //salir();
-    }
-
-    if(int_show == 0){
-        //show();
-    }
-
-    if(int_quit == 0){
-        printf("See you later");
-        return 0;
-    }
-
-*/
     return 0;
 }
